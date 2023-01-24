bb082421dce234530b93b4d5a45b2845c8632273
Sérgio Luiz Resende <sergio.resende@gmail.com>
2023-01-24 10:15:28 -0300

    Criação do Projeto

---
+++ transaction-bff/src/test/java/br/com/resoft/transactionbff/TransactionBffApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.resoft.transactionbff;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TransactionBffApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
